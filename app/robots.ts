70b2add1213d02aa1539e28262c0ea82a4e4d932
Muhammed Alaa <mohamed.alaa7785@gmail.com>
2026-02-16 12:20:02 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,11 @@
+import type { MetadataRoute } from 'next'
 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/'
+    },
+    sitemap: 'https://hiddennarratives.vercel.app/sitemap.xml'
+  }
+}
