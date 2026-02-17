430870c95b929f409369eeec29365c666f5cc4c4
Muhammed Alaa <mohamed.alaa7785@gmail.com>
2026-02-17 06:46:58 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,13 @@ import { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://YOUR_DOMAIN/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap:
+      "https://mohamedalaa7785-cpu-hidden-narratives-qobpb4e5w-hamo-projects.vercel.app/sitemap.xml",
   }
 }
