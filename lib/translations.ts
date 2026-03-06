67b5e7605707c76a8fa589fc2e128b9e368989ca
Muhammed Alaa <mohamed.alaa7785@gmail.com>
2026-03-06 05:48:50 +0200

    Create translations.ts

---
+++ lib/translations.ts
@@ -0,0 +1,19 @@
+export const translations = {
+  en: {
+    home: "Home",
+    episodes: "Episodes",
+    videos: "Videos",
+    about: "About",
+    contact: "Contact",
+    language: "العربية"
+  },
+
+  ar: {
+    home: "الرئيسية",
+    episodes: "الحلقات",
+    videos: "الفيديوهات",
+    about: "حول الموقع",
+    contact: "اتصل بنا",
+    language: "English"
+  }
+};
